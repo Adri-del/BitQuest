87e510ada143160ab181e66553f9449f3d59ef53
Adri <chiquitodiazdeleonadriana@gmail.com>
2026-06-01 19:10:51 -0600

    Definicion de Bases de la clase Mapa

--- rutinas.asm
+++ rutinas.asm
@@ -1,1 +1,33 @@
 // Funciones implementadas en NASM
+
+// Funciones Obligatorias
+// Funciones NASM
+
+ContarCaracter(direccion inicial del mapa, 
+                numero total de celdas, 
+                el carater que se desea contar)
+    // Su principal funcion es contar cuantas monedas existen en cada nivel
+    // Calcula las monedas
+
+validarMovimiento(direccion inicial del mapa, 
+                    numero de col del mapa, 
+                    la nueva fila propuesta para el jugador, 
+                    la nueva columna propuesta para el jugador)
+    // Valida si una posicion en el mapa puede serr ocupada por el jugaador
+    // Devera devolver 1 (mov valido) o 0 (mov invelido)
+
+calcularPuntaje(Monedas recolectadas,
+                pasos realizados,
+                niveles completados)
+    // El resultado debe de mostrarse en el programa princial
+
+detectarObjeto(direccion inicial del mapa, 
+                numero de columnas del mapa,
+                fila a revisar,
+                columna a revisar,
+                el caracter del objeto que se desea buscar )
+    // Debera devolver 1 (el objeto buscado se encuentra en la celda indicada) o 0 (el objeto buscado no se en cuentra en la celda indicada)
+
+contarCeldasLibres(direccion inicial del mapa, num total de celdas)
+    // Debera recorrer el mapa y contar los caminos libres
+
